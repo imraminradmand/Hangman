fa07c6acee558a0abafbeaef5024dd7f07d17ea4
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2023-02-09 15:50:39 -0700

    error handling imporvement + formatting

--- src/AccountHandler.java
+++ src/AccountHandler.java
@@ -1,132 +1,147 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class AccountHandler implements Runnable{
-    private Socket socket;
-    public AccountHandler(Socket socket){
-        this.socket = socket;
+public class AccountHandler implements Runnable {
+
+  private final Socket socket;
+
+  public AccountHandler(Socket socket) {
+    this.socket = socket;
+  }
+
+  /*
+   * Main function
+   *
+   * The goal of this function is to handle the server requests by utilizing file IO.
+   *
+   *     The protocol that is implemented follows as:
+   * GET <username> <password> - The return statement pass through the socket will either be the user's score or if the account does not exist
+   *                            it will return !noaccount!
+   * POST <username> <password> <score> - This will return !success! if it is successful, if it fails there is no output.
+   *
+   * */
+  @Override
+  public void run() {
+    try {
+      InputStreamReader socketInputReader = new InputStreamReader(socket.getInputStream());
+      OutputStream socketOutStream = socket.getOutputStream();
+
+      BufferedReader socketInput = new BufferedReader(socketInputReader);
+      PrintWriter socketOutput = new PrintWriter(socketOutStream, true);
+
+      while (true) {
+        String clientResponse = socketInput.readLine();
+        System.out.println(clientResponse);
+        if (clientResponse != null) {
+          String[] clientArgs = clientResponse.split(" ");
+
+          if (clientArgs[0].equals("exit")) {
+            break;
+          }
+
+          socketOutput.println(fileIO(clientArgs));
+        }
+      }
+      socketOutput.close();
+      socketInput.close();
+      socket.close();
+    } catch (IOException e) {
+      System.err.println(
+          "Error occurred while reading from or writing to the socket: " + e.getMessage());
+      try {
+        socket.close();
+      } catch (IOException closeException) {
+        System.err.println(
+            "Error occurred while trying to close the socket: " + closeException.getMessage());
+      }
+    }
+  }
+
+  /*
+   * METHOD: fileIO
+   *
+   * Purpose: The main function that controls thread synchronization for file IO (makes sure that only one thread can write or read at once).
+   *
+   * */
+  private synchronized String fileIO(String[] clientArgs) throws IOException {
+    if (clientArgs[0].equalsIgnoreCase("get")) {
+      return readFromFile(clientArgs);
+    } else {
+      return writeToFile(clientArgs);
+    }
+  }
+
+  /*
+   * METHOD: readFrmFile
+   *
+   * Purpose: The goal of the method is to provide capability to read from the file. This will return the data for a specified user account or it will
+   *          return an invalid account.
+   * */
+  private String readFromFile(String[] clientArgs) throws FileNotFoundException {
+    File myObj = new File("users.txt");
+    Scanner myReader = new Scanner(myObj);
+    String result = "!noaccount!";
+    while (myReader.hasNextLine()) {
+      String data = myReader.nextLine();
+      String[] arg = data.split(" ");
+
+      if (arg[0].equals(clientArgs[1]) && arg[1].equals(clientArgs[2])) {
+        result = data;
+        break;
+      }
+    }
+    myReader.close();
+    return result;
+  }
+
+  /*
+   * METHOD: writeToFile
+   *
+   * GOAL: The purpose of this file is to write an account and score to the file. The file is copied, deleted and recreated with the contents plus
+   *       the specified client arguments.
+   *
+   * */
+  private String writeToFile(String[] clientArgs) throws IOException {
+    File file = new File("users.txt");
+    Scanner myReader = new Scanner(file);
+    ArrayList<String> lines = new ArrayList<>();
+    while (myReader.hasNextLine()) {
+      String data = myReader.nextLine();
+      lines.add(data);
+
+    }
+    myReader.close();
+    for (String line : lines) {
+      String[] arg = line.split(" ");
+      if (arg[0].equals(clientArgs[1])) {
+        lines.remove(line);
+        break;
+      }
     }
 
-    /*
-    * Main function
-    *
-    * The goal of this function is to handle the server requests by utilizing file IO.
-    *
-    *     The protocol that is implemented follows as:
-    * GET <username> <password> - The return statement pass through the socket will either be the user's score or if the account does not exist
-    *                            it will return !noaccount!
-    * POST <username> <password> <score> - This will return !success! if it is successful, if it fails there is no output.
-    *
-    * */
-    @Override
-    public void run() {
-        try {
-            InputStreamReader socketInputReader = new InputStreamReader(socket.getInputStream());
-            OutputStream socketOutStream = socket.getOutputStream();
+    if (file.delete()) {
+      file.createNewFile();
 
-            BufferedReader socketInput = new BufferedReader(socketInputReader);
-            PrintWriter socketOutput = new PrintWriter(socketOutStream, true);
+      FileWriter writer = new FileWriter("users.txt");
+      lines.add(clientArgs[1] + " " + clientArgs[2] + " " + clientArgs[3]);
 
-            while (true) {
-                String clientResponse = socketInput.readLine();
-                System.out.println(clientResponse);
-                if (clientResponse != null) {
-                    String[] clientArgs = clientResponse.split(" ");
+      for (String s : lines) {
+        writer.write(s + '\n');
+      }
+      writer.close();
 
-                    if (clientArgs[0].equals("exit")){
-                        break;
-                    }
-
-                    socketOutput.println(fileIO(clientArgs));
-                }
-            }
-            socketOutput.close();
-            socketInput.close();
-            socket.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+      return ("!success!");
+    } else {
+      return ("!fail!");
     }
-
-    /*
-    * METHOD: fileIO
-    *
-    * Purpose: The main function that controls thread synchronization for file IO (makes sure that only one thread can write or read at once).
-    *
-    * */
-    private synchronized String fileIO(String[] clientArgs) throws IOException {
-        if(clientArgs[0].equalsIgnoreCase("get")){
-            return readFromFile(clientArgs);
-        }else {
-            return writeToFile(clientArgs);
-        }
-    }
-
-    /*
-    * METHOD: readFrmFile
-    *
-    * Purpose: The goal of the method is to provide capability to read from the file. This will return the data for a specified user account or it will
-    *          return an invalid account.
-    * */
-    private String readFromFile(String[] clientArgs) throws FileNotFoundException {
-        File myObj = new File("users.txt");
-        Scanner myReader = new Scanner(myObj);
-        String result = "!noaccount!";
-        while (myReader.hasNextLine()) {
-            String data = myReader.nextLine();
-            String[] arg = data.split(" ");
-
-            if (arg[0].equals(clientArgs[1]) && arg[1].equals(clientArgs[2])) {
-                result = data;
-                break;
-            }
-        }
-        myReader.close();
-        return result;
-    }
-
-    /*
-    * METHOD: writeToFile
-    *
-    * GOAL: The purpose of this file is to write an account and score to the file. The file is copied, deleted and recreated with the contents plus
-    *       the specified client arguments.
-    *
-    * */
-    private String writeToFile(String[] clientArgs) throws IOException {
-        File file = new File("users.txt");
-        Scanner myReader = new Scanner(file);
-        ArrayList<String> lines = new ArrayList<>();
-        while (myReader.hasNextLine()) {
-            String data = myReader.nextLine();
-            lines.add(data);
-
-        }
-        myReader.close();
-        for (String line : lines) {
-            String[] arg = line.split(" ");
-            if (arg[0].equals(clientArgs[1])) {
-                lines.remove(line);
-                break;
-            }
-        }
-
-        if (file.delete()) {
-            file.createNewFile();
-
-
-            FileWriter writer = new FileWriter("users.txt");
-            lines.add(clientArgs[1] + " " + clientArgs[2] + " " + clientArgs[3]);
-
-            for (String s : lines) {
-                writer.write(s + '\n');
-            }
-            writer.close();
-
-            return ("!success!");
-        } else {
-            return ("!fail!");
-        }
-    }
+  }
 }
--- src/AccountsService.java
+++ src/AccountsService.java
@@ -1,8 +1,6 @@
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -16,27 +14,31 @@ public class AccountsService {
     When a client is accepted the socket is passed into a separate thread so that file IO can be properly handled.
 
      */
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket;
 
-        File file = new File("users.txt");
-        file.createNewFile();
+  private static final int PORT = 7777;
 
-        try {
+  public static void main(String[] args) throws IOException {
+    ServerSocket serverSocket;
 
-            serverSocket = new ServerSocket(7777);
+    File file = new File("users.txt");
+    file.createNewFile();
 
-            System.out.println("Server is running...");
-            ExecutorService fixedThreadPool = Executors.newFixedThreadPool(10);
+    try {
 
-            while (true) {
-               fixedThreadPool.execute(new AccountHandler(serverSocket.accept()));
-            }
-        } catch (IOException e) {
-            System.out.println(
-                    "Exception caught when trying to listen on port " + 7777 + " or listening for a connection");
-            System.out.println(e.getMessage());
-        }
+      serverSocket = new ServerSocket(PORT);
+
+      System.out.println("Server is running...");
+      ExecutorService fixedThreadPool = Executors.newFixedThreadPool(10);
+
+      while (true) {
+        fixedThreadPool.execute(new AccountHandler(serverSocket.accept()));
+      }
+    } catch (IOException e) {
+      System.out.println(
+          "Exception caught when trying to listen on port " + PORT
+              + " or listening for a connection");
+      System.out.println(e.getMessage());
     }
+  }
 
 }
