3bab1639b57c2f108ce2c4fa7e13269577aafe0f
greeves12 <tategreeves@gmail.com>
2023-02-26 13:06:46 -0700

    Added exceptions to interface

--- A2/src/AccountInterface.java
+++ A2/src/AccountInterface.java
@@ -1,8 +1,9 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface AccountInterface extends Remote {
-    public boolean writeToFile(String[] args) throws IOException;
-    public String readFromFile(String[] args);
+    public boolean writeToFile(String[] args) throws IOException, RemoteException;
+    public String readFromFile(String[] args) throws RemoteException;
 }
